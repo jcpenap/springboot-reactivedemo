8b83ba2fc1ce3d755fd0d7a1a04cc71e5963ea61
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2021-11-24 12:51:41 -0500

    Using a Subscriber

--- src/test/java/com/bahrath/springboot/reactive/ReactivedemoApplicationTests.java
+++ src/test/java/com/bahrath/springboot/reactive/ReactivedemoApplicationTests.java
@@ -1,6 +1,8 @@
 package com.bahrath.springboot.reactive;
 
 import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 import org.springframework.boot.test.context.SpringBootTest;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -33,6 +35,37 @@ class ReactivedemoApplicationTests {
 				.log()
 				.map(data -> data.toUpperCase())
 				.subscribe(new OrderConsumer());
+
+		Thread.sleep(7000);
+	}
+
+	@Test
+	void testFluxWithSubscriber() throws InterruptedException {
+		Flux.fromIterable(Arrays.asList("Mackbook Pro", "Iphone", "Dell"))
+				.delayElements(Duration.ofSeconds(2))
+				.log()
+				.map(data -> data.toUpperCase())
+				.subscribe(new Subscriber<String>() {
+					@Override
+					public void onSubscribe(Subscription subscription) {
+						subscription.request(Long.MAX_VALUE);
+					}
+
+					@Override
+					public void onNext(String order) {
+						System.out.println(order);
+					}
+
+					@Override
+					public void onError(Throwable t) {
+						t.printStackTrace();
+					}
+
+					@Override
+					public void onComplete() {
+						System.out.println("Completely Done!!");
+					}
+				});
 
 		Thread.sleep(7000);
 	}
