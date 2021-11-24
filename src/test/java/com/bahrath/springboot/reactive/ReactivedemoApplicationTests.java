1e21defdde646a02557b4da7430a1f9947558ebe
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2021-11-24 10:52:20 -0500

    first commit

---
+++ src/test/java/com/bahrath/springboot/reactive/ReactivedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bahrath.springboot.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactivedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
