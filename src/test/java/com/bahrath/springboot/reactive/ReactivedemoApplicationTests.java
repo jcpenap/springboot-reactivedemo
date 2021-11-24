dea3aee28a6aeb750d58846295a1be251d37a5f4
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2021-11-24 11:38:45 -0500

    Adding Mono and Flux Test

--- src/test/java/com/bahrath/springboot/reactive/ReactivedemoApplicationTests.java
+++ src/test/java/com/bahrath/springboot/reactive/ReactivedemoApplicationTests.java
@@ -2,12 +2,25 @@ package com.bahrath.springboot.reactive;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @SpringBootTest
 class ReactivedemoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testMono() {
+		Mono<String> mono = Mono.just("Mackbook Pro");
+		mono.log().map(data -> data.toUpperCase())
+				.subscribe(data -> System.out.println(data));
+	}
+
+	@Test
+	void testFlux() {
+		Flux.just("Mackbook Pro", "Iphone", "Dell")
+				.log()
+				.map(data -> data.toUpperCase())
+				.subscribe(data -> System.out.println(data));
 	}
 
 }
