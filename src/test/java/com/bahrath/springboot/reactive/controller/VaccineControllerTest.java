4a125e703c8475c3e39cf0a3a73c4cc4a9be7759
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2021-11-26 09:03:37 -0500

    Adding reactive unit test controller

---
+++ src/test/java/com/bahrath/springboot/reactive/controller/VaccineControllerTest.java
@@ -0,0 +1,36 @@
+package com.bahrath.springboot.reactive.controller;
+
+import com.bahrath.springboot.reactive.vaccine.Vaccine;
+import com.bahrath.springboot.reactive.vaccine.VaccineService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class VaccineControllerTest {
+
+    @Autowired
+    private VaccineController controller;
+
+    @MockBean
+    private VaccineService service;
+
+    @Test
+    void getVaccines() {
+        when(service.getVaccines()).thenReturn(Flux.just(Vaccine.builder().name("Pfizer").build(),
+                Vaccine.builder().name("J&J").build(),
+                Vaccine.builder().name("Covaxin").build()));
+
+        StepVerifier.create(controller.getVaccines())
+                .expectNextCount(3)
+                .expectComplete()
+                .verify();
+
+    }
+}
